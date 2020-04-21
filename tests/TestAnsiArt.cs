e7e52c5bf5103dfebcc6a4944ab350087ada2a93
Charlie Kindel <charlie@kindel.com>
2020-04-20 21:22:25 -0600

    Upgraded so everything acutally works with dotnetcore

--- tests/TestAnsiArt.cs
+++ tests/TestAnsiArt.cs
@@ -12,59 +12,60 @@ namespace libVT100.Tests
     public class TestAnsiArt
     {
         [Test]
-        public void TestWendy ()
+        public void TestWendy()
         {
-           ReadAndRenderFile ( "tests/70-twilight.ans", Encoding.GetEncoding ( "ibm437"), new Size(80, 80) );
-           ReadAndRenderFile ( "tests/n4-wendy.ans", Encoding.GetEncoding ( "ibm437"), new Size(80, 80)  );
-           ReadAndRenderFile( "tests/zv-v01d.ans", Encoding.GetEncoding( "ibm437" ), new Size( 80, 180 ) );
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            ReadAndRenderFile("../../../70-twilight.ans", CodePagesEncodingProvider.Instance.GetEncoding("ibm437"), new Size(80, 80));
+            ReadAndRenderFile("../../../n4-wendy.ans", CodePagesEncodingProvider.Instance.GetEncoding("ibm437"), new Size(80, 80));
+            ReadAndRenderFile("../../../zv-v01d.ans", CodePagesEncodingProvider.Instance.GetEncoding("ibm437"), new Size(80, 180));
         }
-        
+
         [Test]
-        public void TestSimpleTxt ()
+        public void TestSimpleTxt()
         {
-            System.Console.Write ( ReadAndRenderFile ( "tests/simple.txt", Encoding.UTF8, new Size(50, 6) ).ToString() );
+            System.Console.Write(ReadAndRenderFile("../../../simple.txt", Encoding.UTF8, new Size(50, 6)).ToString());
         }
-        
+
         [Test]
-        public void TestUnixProgramOutput ()
+        public void TestUnixProgramOutput()
         {
-           ReadAndRenderFile ( "tests/mc.output", Encoding.UTF8, new Size(180, 65) );
-           ReadAndRenderFile ( "tests/ls.output", Encoding.UTF8, new Size(65, 10) );
+            ReadAndRenderFile("../../../mc.output", Encoding.UTF8, new Size(180, 65));
+            ReadAndRenderFile("../../../ls.output", Encoding.UTF8, new Size(65, 10));
         }
-        
-        public void ReadAndRenderFileAll ( string _filename, Size _size )
+
+        public void ReadAndRenderFileAll(string _filename, Size _size)
         {
-            foreach ( EncodingInfo encodingInfo in Encoding.GetEncodings() )
+            foreach (EncodingInfo encodingInfo in Encoding.GetEncodings())
             {
-                ReadAndRenderFile ( _filename, encodingInfo.GetEncoding(), _size );
+                ReadAndRenderFile(_filename, encodingInfo.GetEncoding(), _size);
             }
         }
-        
-        public Screen ReadAndRenderFile ( string _filename, Encoding _encoding, Size _size )
+
+        public Screen ReadAndRenderFile(string _filename, Encoding _encoding, Size _size)
         {
             IAnsiDecoder vt100 = new AnsiDecoder();
             //vt100.Encoding = Encoding.GetEncoding ( encodingInfo.Name, new EncoderExceptionFallback(), new DecoderReplacementFallback ("U") );
             vt100.Encoding = _encoding;
-            Screen screen = new Screen ( _size.Width, _size.Height );
-            vt100.Subscribe ( screen );
-           
-            using ( Stream stream = File.Open( _filename, FileMode.Open ) )
+            Screen screen = new Screen(_size.Width, _size.Height);
+            vt100.Subscribe(screen);
+
+            using (Stream stream = File.Open(_filename, FileMode.Open))
             {
                 try
                 {
                     int read = 0;
-                    while ( ( read = stream.ReadByte()) != -1 )
+                    while ((read = stream.ReadByte()) != -1)
                     {
-                        vt100.Input ( new byte[] { (byte) read } );
+                        vt100.Input(new byte[] { (byte)read });
                     }
                 }
-                catch ( EndOfStreamException )
+                catch (EndOfStreamException)
                 {
                 }
             }
             //System.Console.Write ( screen.ToString() );
-            Bitmap bitmap = screen.ToBitmap ( new Font("Courier New", 6) );
-            bitmap.Save ( "build\\" + Path.GetFileNameWithoutExtension(_filename) + "_" + _encoding.EncodingName + ".png", System.Drawing.Imaging.ImageFormat.Png );
+            Bitmap bitmap = screen.ToBitmap(new Font("Courier New", 6));
+            bitmap.Save(Path.GetFileNameWithoutExtension(_filename) + "_" + _encoding.EncodingName + ".png", System.Drawing.Imaging.ImageFormat.Png);
 
             /*
               foreach ( Screen.Character ch in screen )
